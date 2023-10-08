83b066c4500c5f3acdc9e61ef6e4a3b676c77df7
Christian Lauinger <christian.lauinger@gmail.com>
2023-10-08 19:27:24 +0200

    Fix typos / update translation
    Found via `codespell -S po -L doubleclick`

--- SettingsCenter@lauinger-clan.de/translation.js
+++ SettingsCenter@lauinger-clan.de/translation.js
@@ -1,7 +1,7 @@
 import { gettext as _ } from "resource:///org/gnome/shell/extensions/extension.js";
 
 /*
-the next lines are only here to be catched by translation
+the next lines are only here to be caught by translation
 this is is not packed with the extension or installed - only purpose is to catch the string and add them to po file by update-translation-po-files.sh
 */
 const strFix1 = _("Gnome Tweaks");
