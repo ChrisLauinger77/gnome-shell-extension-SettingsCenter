f61877d239c01c72fbeceefbf469d8fcef75ca67
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-29 17:00:26 +0200

    Fixes extension installation and item parsing
    Corrects the extension installation process by using the correct zip file name, ensuring proper installation and activation.
    
    Simplifies menu item parsing by directly accessing the split data, improving code readability and maintainability.

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@
 cd SettingsCenter\@lauinger-clan.de
 gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=menu_items.js --extra-source=../LICENSE
 cd ..
-mv SettingsCenter@lauinger-clan.de.shell-extension.zip SettingsCenter@lauinger-clan.de.zip
 
 if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
    echo "Extension zip created ..."
 else
-   gnome-extensions install SettingsCenter\@lauinger-clan.de.zip --force
-   gnome-extensions enable SettingsCenter\@lauinger-clan.de
+   gnome-extensions install SettingsCenter\@lauinger-clan.de.shell-extension.zip --force
+   gnome-extensions enable SettingsCenter\@lauinger-clan.shell-extension.de
 fi
